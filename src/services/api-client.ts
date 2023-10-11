e187b843b2c13c7026f2c130cea32baa31123b48
janvi-davda <janvi.davda@indexnine.com>
2023-10-12 03:18:37 +0530

    Seperation of concerns by creating fetching games custom hook

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
     baseURL: 'https://api.rawg.io/api/',
     params:{
-        key: 'a59ca92cf6ab4143849bc8921febf49f'
+        key: 'a59ca92cf6ab4143849bc8921febf49f' // added temporarily - generally to be stored as environment variable for security purpose
     }
 })
