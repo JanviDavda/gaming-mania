185570c0ca0006082a65f0328fbd8a99b3bf90ea
janvi-davda <janvi.davda@indexnine.com>
2023-10-14 00:37:20 +0530

    Created generic custom hook for data fetching

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -5,16 +5,12 @@ import GameCardSkeleton from "./GameCardSkeleton";
 import GameCardContainer from "./GameCardContainer";
 
 const GameGrid = () => {
-  const { games, error, isLoading } = useGames();
+  const { data: games, error, isLoading } = useGames();
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   return (
     <>
       {error && <Text>{error}</Text>}
-      <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
-        spacing={10}
-        padding="10px"
-      >
+      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3 }} spacing={10} padding="10px">
         {isLoading &&
           skeletons.map((skeleton) => (
             <GameCardContainer>
