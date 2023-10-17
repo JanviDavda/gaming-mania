17e9532eef6da440c1e986b4e0f8a0c3a5ff629a
janvi-davda <janvi.davda@indexnine.com>
2023-10-17 15:56:16 +0530

    Added platform selector component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Grid, GridItem, Show } from "@chakra-ui/react";
 import Navbar from "./components/Navbar";
 import GameGrid from "./components/GameGrid";
 import GenreList from "./components/GenreList";
+import PlatformSelector from "./components/platformSelector";
 import { Genre } from "./hooks/useGenres";
 
 const App = () => {
@@ -30,6 +31,7 @@ const App = () => {
         </GridItem>
       </Show>
       <GridItem area="main">
+        <PlatformSelector />
         <GameGrid selectedGenre={selectedGenre} />
       </GridItem>
     </Grid>
