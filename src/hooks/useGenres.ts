6cbc7923e0c45d86769cc8b30d2c44f0e8050535
janvi-davda <janvi.davda@indexnine.com>
2023-10-18 16:51:27 +0530

    Ship genres with the app and making genres data static

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,4 +1,5 @@
-import useData from "./useData";
+// import useData from "./useData";  --> for fetching data dynamically from backend(API)
+import genres from "../data/genres";
 
 export interface Genre {
     id: number;
@@ -7,6 +8,6 @@ export interface Genre {
 }
 
 
-const useGenres = () => useData<Genre>('/genres');
-
+// const useGenres = () => useData<Genre>('/genres'); --> for fetching data dynamically from backend(API)
+const useGenres = () => ({data: genres, isLoading: false, error: null}) //--> for fetching data statically from local storage
 export default useGenres;
